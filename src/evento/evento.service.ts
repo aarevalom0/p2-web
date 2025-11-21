8fecd577735f8ae2dd529691b743270655f20462
aarevalom0 <a.arevalom@uniandes.edu.co>
2025-11-21 08:57:35 -0500

    upload evento service

--- src/evento/evento.service.ts
+++ src/evento/evento.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
-import { CreateEventoDto } from './dto/create-evento.dto';
-import { UpdateEventoDto } from './dto/update-evento.dto';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MongoRepository } from 'typeorm';
+import { EventoDto } from './dto/evento.dto';
+import { Evento } from './entities/evento.entity';
 
 @Injectable()
 export class EventoService {
-  create(createEventoDto: CreateEventoDto) {
-    return 'This action adds a new evento';
+  constructor(
+    @InjectRepository(Evento)
+    private readonly eventoRepository: MongoRepository<Evento>,
+  ) {}
+
+  crearEvento(createEventoDto: Evento) {
+    const { duracionHoras, ponente, descripcion } = createEventoDto;
+    if (duracionHoras <= 0) {
+      throw new Error('La duración debe ser positiva.');
+    }
+    if (ponente.tipoPonente === 'Invitado') {
+      if (descripcion.length < 50) {
+        throw new Error(
+          'La descripción debe tener al menos 50 caracteres para ponentes invitados.',
+        );
+      }
+    }
+    const nuevoEvento = this.eventoRepository.create(createEventoDto);
+    return this.eventoRepository.save(nuevoEvento);
   }
 
-  findAll() {
-    return `This action returns all evento`;
+  async aprobarEvento(id: number) {
+    const evento = await this.eventoRepository.findOne({ where: { id: id } });
+    if (!evento) {
+      throw new NotFoundException(`Evento con ID ${id} no encontrado`);
+    }
+    if (!evento.auditorio) {
+      throw new BadRequestException(
+        'Solo puede aprobarse un evento con auditorio asignado',
+      );
+    }
+    evento.estado = 'Aprobado';
+    return this.eventoRepository.save(evento);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} evento`;
+  async eliminarEvento(id: number) {
+    const evento = await this.eventoRepository.findOne({ where: { id: id } });
+    if (!evento) {
+      throw new NotFoundException(`Evento con ID ${id} no encontrado`);
+    }
+    if (evento.estado === 'Aprobado') {
+      throw new BadRequestException('No se puede eliminar un evento aprobado');
+    }
+    return this.eventoRepository.delete(id);
   }
 
-  update(id: number, updateEventoDto: UpdateEventoDto) {
-    return `This action updates a #${id} evento`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} evento`;
+  findEventoById(id: number) {
+    return this.eventoRepository.findOne({ where: { id: id } });
   }
 }

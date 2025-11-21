b1f165441ed3514969425f7f154dc5ca194ee5a0
aarevalom0 <a.arevalom@uniandes.edu.co>
2025-11-21 09:21:10 -0500

    controller del evento

--- src/evento/evento.controller.ts
+++ src/evento/evento.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { EventoService } from './evento.service';
 import { Evento } from './entities/evento.entity';
 
@@ -6,5 +6,23 @@ import { Evento } from './entities/evento.entity';
 export class EventoController {
   constructor(private readonly eventoService: EventoService) {}
 
-  
+  @Post()
+  crearEvento(@Body() createEventoDto: Evento) {
+    return this.eventoService.crearEvento(createEventoDto);
+  }
+
+  @Post(':id/aprobar')
+  aprobarEvento(@Param('id') id: number) {
+    return this.eventoService.aprobarEvento(id);
+  }
+
+  @Delete(':id')
+  eliminarEvento(@Param('id') id: number) {
+    return this.eventoService.eliminarEvento(id);
+  }
+
+  @Get(':id')
+  findEventoById(@Param('id') id: number) {
+    return this.eventoService.findEventoById(id);
+  }
 }
